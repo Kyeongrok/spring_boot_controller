871a61be3c2bf833d2b81c60e1e59b85e939553a
mattheu <mattheu@plutusds.com>
2018-11-30 15:13:59 +0900

    first commit

---
+++ src/main/java/com/test/controllers/OrderController.java
@@ -0,0 +1,16 @@
+package com.test.controllers;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api/v1/order", produces = {MediaType.APPLICATION_JSON_UTF8_VALUE})
+public class OrderController {
+
+    @GetMapping(value = "/")
+    public String getWalletSummaries() {
+        return "success";
+    }
+}
